86b6113ec571537c21da9a210dd38e031ed58494
Alvaro Pajaro <alvaroe.pajaro@gmail.com>
2022-04-27 18:40:48 -0500

    Fix guides

--- src/components/Guide/JupiterUniques.js
+++ src/components/Guide/JupiterUniques.js
@@ -41,8 +41,14 @@ const Img = styled('img')(({ theme }) => ({
 const UniquesContainer = styled(Grid)(({ theme }) => ({
   position: 'relative',
   [theme.breakpoints.down('md')]: {
-    minHeight: '350px',
+    minHeight: '550px',
     minWidth: '100%'
+  },
+  [theme.breakpoints.down('sm')]: {
+    minHeight: '450px'
+  },
+  [theme.breakpoints.down('xs')]: {
+    minHeight: '350px'
   }
 }));
 
@@ -50,9 +56,15 @@ const BaalImg = styled(Img)(({ theme }) => ({
   top: 0,
   left: -100,
   width: '60%',
+  [theme.breakpoints.down('md')]: {
+    width: '40%'
+  },
   [theme.breakpoints.down('sm')]: {
     width: '80%',
-    left: -80
+    left: -130
+  },
+  [theme.breakpoints.down('xs')]: {
+    left: -90
   }
 }));
 
@@ -60,10 +72,18 @@ const YunoImg = styled(Img)(({ theme }) => ({
   top: 230,
   right: 40,
   zIndex: '3',
+  [theme.breakpoints.down('md')]: {
+    left: 450
+  },
   [theme.breakpoints.down('sm')]: {
     top: 150,
-    right: -40,
-    width: '70%'
+    left: 250,
+    width: '50%'
+  },
+  [theme.breakpoints.down('xs')]: {
+    top: 120,
+    width: '70%',
+    left: 130
   }
 }));
 
@@ -71,10 +91,16 @@ const EarthImg = styled(Img)(({ theme }) => ({
   top: 200,
   left: 40,
   zIndex: '2',
+  [theme.breakpoints.down('md')]: {
+    left: 150
+  },
   [theme.breakpoints.down('sm')]: {
-    left: -20,
-    top: 120,
-    width: '80%'
+    left: 100,
+    top: 160,
+    width: '50%'
+  },
+  [theme.breakpoints.down('xs')]: {
+    left: 20
   }
 }));
 
@@ -82,10 +108,19 @@ const JupiterImg = styled(Img)(({ theme }) => ({
   top: -200,
   right: -180,
   width: '100%',
+  [theme.breakpoints.down('md')]: {
+    right: -300,
+    top: -250,
+    width: '90%'
+  },
   [theme.breakpoints.down('sm')]: {
-    top: -100,
-    right: -100,
+    top: -160,
+    right: -190,
     width: '120%'
+  },
+  [theme.breakpoints.down('xs')]: {
+    right: -120,
+    top: -100
   }
 }));
 
@@ -93,8 +128,18 @@ const ZielImg = styled(Img)(({ theme }) => ({
   top: 50,
   left: 200,
   width: '35%',
+  [theme.breakpoints.down('md')]: {
+    top: -10,
+    left: 250,
+    width: '30%'
+  },
   [theme.breakpoints.down('sm')]: {
-    left: 120
+    left: 150,
+    width: '35%'
+  },
+  [theme.breakpoints.down('xs')]: {
+    width: '40%',
+    left: 100
   }
 }));
 
@@ -119,9 +164,20 @@ const JupiterUniques = () => {
           <ZielImg src={zielImg} alt="power-level-uniques" />
         </UniquesContainer>
         <Grid item s={12} lg={5} style={{ marginBottom: '2em' }}>
-          <Text>{t('jupiter.uniques.guide.text1')}</Text>
-          <div style={{ display: 'flex' }}>
-            <div style={{ width: '50%', padding: '0.5em' }}>
+          <Grid item lg={12} style={{ marginBottom: '2em' }}>
+            <Text>{t('jupiter.uniques.guide.text1')}</Text>
+          </Grid>
+
+          <Grid container lg={12}>
+            <Grid
+              item
+              xs={6}
+              s={6}
+              md={6}
+              lg={6}
+              justifyContent="center"
+              style={{ display: 'flex', alignItems: 'center', justifyItems: 'center' }}
+            >
               <ul style={{ listStyle: 'none', margin: 0, padding: '0 0 0 1em', textAlign: 'left', fontSize: '1.5em' }}>
                 <li>{t('reverse.scrolls')}</li>
                 <li>{t('immortals.11D')}</li>
@@ -134,17 +190,49 @@ const JupiterUniques = () => {
                 <li>{t('dimension.hole')}</li>
                 <li>{t('move.speed.150')}</li>
               </ul>
-            </div>
-            <div style={{ width: '50%', padding: '0.5em', marginLeft: '1em', display: 'grid' }}>
-              <img src={arenaImg} alt={'arena'} style={{ minHeight: '40px', maxWidth: '100%' }} />
-              <img src={astralImg} alt={'astral'} style={{ minHeight: '40px', maxWidth: '100%' }} />
-              <img src={dimensionImg} alt={'dimension'} style={{ minHeight: '40px', maxWidth: '100%' }} />
-              <img src={globalImg} alt={'global'} style={{ minHeight: '40px', maxWidth: '100%' }} />
-              <img src={potsImg} alt={'pots'} style={{ minHeight: '40px', maxWidth: '100%' }} />
-              <img src={reverseImg} alt={'reverse'} style={{ minHeight: '40px', maxWidth: '100%' }} />
-              <img src={speedImg} alt={'speed'} style={{ minHeight: '40px', maxWidth: '100%' }} />
-            </div>
-          </div>
+            </Grid>
+            <Grid
+              item
+              xs={6}
+              s={6}
+              md={6}
+              lg={6}
+              justifyContent="center"
+              style={{ display: 'flex', alignItems: 'center', justifyItems: 'center' }}
+            >
+              <table>
+                <tr>
+                  <td>
+                    <img src={globalImg} alt={'global'} style={{ minHeight: '40px', maxWidth: '100%' }} />
+                  </td>
+                  <td>
+                    <img src={reverseImg} alt={'reverse'} style={{ minHeight: '40px', maxWidth: '100%' }} />
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <img src={astralImg} alt={'astral'} style={{ minHeight: '40px', maxWidth: '100%' }} />
+                  </td>
+                  <td>
+                    <img src={astralImg} alt={'reverse'} style={{ minHeight: '40px', maxWidth: '100%' }} />
+                  </td>
+                </tr>
+                <tr>
+                  <td colSpan={2}>
+                    <img src={potsImg} alt={'astral'} style={{ minHeight: '40px', maxWidth: '100%' }} />
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <img src={dimensionImg} alt={'astral'} style={{ minHeight: '40px', maxWidth: '100%' }} />
+                  </td>
+                  <td>
+                    <img src={speedImg} alt={'reverse'} style={{ minHeight: '40px', maxWidth: '100%' }} />
+                  </td>
+                </tr>
+              </table>
+            </Grid>
+          </Grid>
         </Grid>
       </Grid>
     </Container>
